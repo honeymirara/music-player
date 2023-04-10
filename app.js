04fe1966af326c380c9c57f41292f91db9c2aef0
honeymirara <honeymirara@gmail.com>
2023-04-10 20:05:31 +0300

    edits

---
+++ app.js
@@ -0,0 +1,42 @@
+let songTitleTag = document.querySelector('.song-title');
+let musicGroupTitleTag = document.querySelector('.music-group');
+let buttonBrendTag = document.querySelector('.button-brend');
+let sliderTag = document.querySelector('.slider');
+let timerTag = document.querySelector('.timer');
+let repeatButtonTag = document.querySelector('.repeat-button');
+let LeftButtonTag = document.querySelector('.buttonLeft');
+let playButtonTag = document.querySelector('.buttonPlay');
+let rightButtonTag = document.querySelector('.buttonRight');
+let albumImageTag = document.querySelector('.album-image');
+
+let flag = false;
+let currentIndexSong = 0;
+
+let playList = [{
+    albumimageTag: ,
+    songTitleTag:,
+    musicGroupTag: ,
+    path:
+},
+{
+    albumimageTag: ,
+    songTitleTag:,
+    musicGroupTag: ,
+    path:
+
+},
+  {
+    albumimageTag: ,
+    songTitleTag:,
+    musicGroupTag: ,
+    path:
+
+}, {
+    albumimageTag: ,
+    songTitleTag:,
+    musicGroupTag: ,
+    path:}]
+
+    buttonPlay.addEventListener('click', ()=>{
+        
+    })
